e858deddaf11e2e4d2ed9c19104735fc87c28fee
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2019-08-30 09:19:41 +0530

    code formatting

--- model/loss.py
+++ model/loss.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+
+
 class LossV2():
     def __init__(self, batch_size=None, n_classes=None):
         self.smooth_l1 = tf.losses.Huber(reduction='none')
@@ -24,24 +26,33 @@ class LossV2():
                  ignore_mask):
         num_positive_detections = tf.maximum(tf.reduce_sum(
             tf.cast(background_mask, dtype=tf.float32), axis=-1), 1.0)
-        
-        positive_classification_mask = tf.expand_dims(tf.logical_not(ignore_mask), axis=-1)
-        positive_classification_mask = tf.tile(positive_classification_mask, multiples=[1, 1, self.num_classes])
-        
+
+        positive_classification_mask = tf.expand_dims(
+            tf.logical_not(ignore_mask), axis=-1)
+        positive_classification_mask = tf.tile(
+            positive_classification_mask, multiples=[1, 1, self.num_classes])
+
         positive_regression_mask = tf.expand_dims(background_mask, axis=-1)
-        positive_regression_mask = tf.tile(positive_regression_mask, multiples=[1, 1, 4])
-        
-        Lcls = self.focal_loss(classification_targets, classification_predictions)
+        positive_regression_mask = tf.tile(
+            positive_regression_mask, multiples=[1, 1, 4])
+
+        Lcls = self.focal_loss(classification_targets,
+                               classification_predictions)
         Lreg = self.smooth_l1(regression_targets, regression_predictions)
         Lcls = Lcls * tf.cast(positive_classification_mask, dtype=tf.float32)
         Lreg = Lreg * tf.cast(positive_regression_mask, dtype=tf.float32)
-        
-        Lcls = tf.reduce_sum(Lcls, axis=[1, 2]).numpy() / num_positive_detections
-        Lreg = tf.reduce_sum(Lreg, axis=[1, 2]).numpy() / num_positive_detections
-        
-        Lcls  = tf.nn.compute_average_loss(Lcls, global_batch_size=self.global_batch_size)
-        Lreg = tf.nn.compute_average_loss(Lreg, global_batch_size=self.global_batch_size)
+
+        Lcls = tf.reduce_sum(
+            Lcls, axis=[1, 2]).numpy() / num_positive_detections
+        Lreg = tf.reduce_sum(
+            Lreg, axis=[1, 2]).numpy() / num_positive_detections
+
+        Lcls = tf.nn.compute_average_loss(
+            Lcls, global_batch_size=self.global_batch_size)
+        Lreg = tf.nn.compute_average_loss(
+            Lreg, global_batch_size=self.global_batch_size)
         return Lcls, Lreg, tf.reduce_mean(num_positive_detections)
+
 
 class Loss():
     def __init__(self, n_classes=None):
